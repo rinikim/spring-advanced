21cc196fb1445bfadbca42d5d4260da9ab74c921
rinikim <tytntu@naver.com>
2022-07-08 23:40:56 +0900

    test

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdvancedApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // test
 		SpringApplication.run(AdvancedApplication.class, args);
 	}
 
